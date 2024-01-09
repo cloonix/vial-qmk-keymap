63af87f57fedae44dda47d6e8ab28b25b36b2851
Claus Malter <3120297+cloonix@users.noreply.github.com>
2024-01-09 12:12:07 +0100

    still figuring it out...

--- keymap.c
+++ keymap.c
@@ -15,15 +15,15 @@
  */
 
 #include QMK_KEYBOARD_H
-#include "sendstring_german.h"
+#include "print.h"
 
 // clang-format off
 enum layers{
   L_01, // 0
   L_02, // 1
   L_03, // 2
-  L_EMOJI, // 3
-  L_GAME // 4
+  L_GAME, // 3
+  L_EMOJI // 4
 };
 
 enum unicode_names {
@@ -46,22 +46,22 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [L_01] = LAYOUT_tkl_iso(
      KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,            KC_PSCR,   KC_TRNS,  RGB_TOG,
-     QK_LEAD,  DE_1,     DE_2,     DE_3,     DE_4,     DE_5,     DE_6,     DE_7,     KC_8,     KC_9,     DE_0,     DE_SS,    DE_ACUT,  KC_BSPC, KC_INS,    KC_HOME,  KC_PGUP,
-     KC_TAB,   DE_Q,     DE_W,     DE_E,     DE_R,     DE_T,     DE_Z,     DE_U,     DE_I,     DE_O,     DE_P,     DE_UDIA,  DE_PLUS,           KC_DEL,    KC_END,   KC_PGDN,
-     MO(L_03), DE_A,     DE_S,     DE_D,     DE_F,     DE_G,     DE_H,     DE_J,     DE_K,     DE_L,     DE_ODIA,  DE_ADIA,  DE_HASH,  KC_ENT,
-     KC_LSFT,  DE_LABK,  DE_Y,     DE_X,     DE_C,     DE_V,     DE_B,     DE_N,     DE_M,     DE_COMM,  DE_DOT,   DE_MINS,            KC_RSFT,            KC_UP,
+     QK_LEAD,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC, KC_INS,    KC_HOME,  KC_PGUP,
+     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,           KC_DEL,    KC_END,   KC_PGDN,
+     MO(L_03), KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS,  KC_ENT,
+     KC_LSFT,  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,            KC_UP,
      KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RGUI, MO(L_02),  KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
 [L_02] = LAYOUT_tkl_iso(
-     KC_TRNS,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  KC_TRNS,  KC_TRNS,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,            RGB_RMOD, RGB_MOD,  RGB_TOG,
+     KC_TRNS,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_SPI,  RGB_SPD,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,            RGB_RMOD, RGB_MOD,  RGB_TOG,
      KC_TRNS,  BT_HST1,  BT_HST2,  BT_HST3,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  RGB_VAI,  RGB_HUI,  RGB_SAI,
      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            RGB_VAD,  RGB_HUD,  RGB_SAD,
      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BAT_LVL,  NK_TOGG,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,            RGB_SPI,
-     KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  RGB_SPD,  KC_TRNS),
+     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BAT_LVL,  NK_TOGG,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,            KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
 
 [L_03] = LAYOUT_tkl_iso(
-     DM_RSTP,  DM_REC1,  DM_REC2,  KC_TRNS,  KC_TRNS,  DM_PLY1,  DM_PLY2,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_PWR,   KC_WAKE,  KC_SLEP,
+     DM_RSTP,  DM_REC1,  DM_PLY1,  DM_REC2,  DM_PLY2,  DB_TOGG,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_PWR,   KC_WAKE,  KC_SLEP,
      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  QK_BOOT,  KC_TRNS,  KC_TRNS,  KC_TRNS,
      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,
      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
@@ -86,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
-// helper functions
+// helper: memory efficient random function
 // https://getreuer.info/posts/keyboards/macros3/index.html#random-emojis
 static uint8_t simple_rand(void) {
   static uint16_t random = 1;
@@ -96,6 +96,11 @@ static uint8_t simple_rand(void) {
 
 // macros
 bool process_record_user(uint16_t keycode, keyrecord_t* record) {
+
+  #ifdef CONSOLE_ENABLE
+    uprintf("KL: kc: 0x%04X, col: %2u, row: %2u, pressed: %u, time: %5u, int: %u, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
+  #endif 
+
   switch (keycode) {
     case UPDIR:  // Types ../ to go up a directory on the shell.
       if (record->event.pressed) {
@@ -116,6 +121,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
         last_index = index;
 
         // Produce the emoji.
+        dprintf("sending emoji %s\n", emojis[index]);
         send_unicode_string(emojis[index]);
       }
       return false;
@@ -127,7 +133,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
 // leader key stuff
 void leader_end_user(void) {
 
-  // switch to game layer (toggle)
+  // toggle game layer (toggle)
   if (leader_sequence_one_key(KC_4)) {
     layer_invert(L_GAME);
 
@@ -146,11 +152,11 @@ void leader_end_user(void) {
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
   for (uint8_t i = led_min; i < led_max; i++) {
     switch(get_highest_layer(layer_state|default_layer_state)) {
-      case 4: // L_GAME
-        rgb_matrix_set_color(i, RGB_RED);
-        break;
-      case 3: // L_EMOJI
+      case 4: // L_EMOJI
         rgb_matrix_set_color(i, RGB_BLUE);
+        break;
+      case 3: // L_GAME
+        rgb_matrix_set_color(i, RGB_RED);
         break;
       case 2: // L_03
         rgb_matrix_set_color(i, RGB_GREEN);
