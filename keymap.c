8329a32253efa0d74c2c805a640b5e1b5eb55f58
Claus Malter <3120297+cloonix@users.noreply.github.com>
2024-07-07 08:58:06 +0000

    minor changes

--- keymap.c
+++ keymap.c
@@ -66,7 +66,7 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [L_01] = LAYOUT_tkl_iso(
     TD(0),   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,                   TD(4),    KC_NO,    RGB_TOG,
-    TD(5),   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,     KC_BSPC,      KC_INS,   KC_HOME,  KC_PGUP,
+    TD(5),   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,     KC_BSPC,      KC_INS,   TD(8),    KC_PGUP,
     TD(6),   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,                  KC_DEL,   KC_END,   KC_PGDN,
     TD(1),   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_NUHS,    KC_ENT,
     KC_LSFT, KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,                KC_UP,
@@ -83,12 +83,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [L_03] = LAYOUT_tkl_iso(
-    DM_RSTP, DM_REC1,        DM_PLY1,     DM_REC2,        DM_PLY2,     DB_TOGG,     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_PWR,   KC_WAKE,  KC_SLEP,
-    KC_NUM,  KC_TRNS,        KC_TRNS,     KC_TRNS,        KC_TRNS,     KC_TRNS,     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  QK_BOOT,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-    KC_TRNS, KC_TRNS,        C(S(KC_W)),  C(S(KC_E)),     C(S(KC_R)),  C(S(KC_T)),  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,
-    KC_TRNS, C(G(KC_LEFT)),  C(S(KC_S)),  C(G(KC_RIGHT)), KC_TRNS,     C(S(KC_G)),  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-    KC_TRNS, KC_TRNS,        KC_TRNS,     KC_TRNS,        KC_TRNS,     KC_TRNS,     KC_WHOM,  KC_TRNS,  KC_MAIL,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,            KC_TRNS,
-    KC_TRNS, KC_TRNS,        KC_TRNS,                                               KC_TRNS,                      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
+    DM_RSTP, DM_REC1,        DM_PLY1,             DM_REC2,        DM_PLY2,     DB_TOGG,     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_PWR,   KC_WAKE,  KC_SLEP,
+    KC_NUM,  KC_TRNS,        KC_TRNS,             KC_TRNS,        KC_TRNS,     KC_TRNS,     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  QK_BOOT,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+    KC_TRNS, KC_TRNS,        C(S(KC_W)),          C(S(KC_E)),     C(S(KC_R)),  C(S(KC_T)),  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,
+    KC_TRNS, C(G(KC_LEFT)),  C(S(KC_CAPS_LOCK)),  C(G(KC_RIGHT)), KC_TRNS,     C(S(KC_G)),  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+    KC_TRNS, KC_TRNS,        KC_TRNS,             KC_TRNS,        KC_TRNS,     KC_TRNS,     KC_WHOM,  KC_TRNS,  KC_MAIL,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,            KC_TRNS,
+    KC_TRNS, KC_TRNS,        KC_TRNS,                                                       KC_TRNS,                      KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS
   ),
 
   [L_EMOJI] = LAYOUT_tkl_iso(
@@ -137,10 +137,8 @@ void keyboard_post_init_user(void) {
   dynamic_keymap_set_tap_dance(6, &td7);
   vial_tap_dance_entry_t td8 = { KC_BSPC, KC_NO, LCTL(KC_BSPC), KC_NO, TAPPING_TERM };
   dynamic_keymap_set_tap_dance(7, &td8);
-//  vial_tap_dance_entry_t td9 = { KC_1, KC_1, KC_1, UM(EXCL), TAPPING_TERM };
-//  dynamic_keymap_set_tap_dance(8, &td9); 
-//  vial_tap_dance_entry_t td10 = { KC_MINS, KC_MINS, KC_MINS, UM(QUES), TAPPING_TERM };
-//  dynamic_keymap_set_tap_dance(9, &td10);
+  vial_tap_dance_entry_t td9 = { KC_HOME, KC_HOME, C(S(KC_CAPS_LOCK)), KC_HOME, TAPPING_TERM };
+  dynamic_keymap_set_tap_dance(8, &td9); 
 
   /*  key overrides (values = bit shift)
   activate when the trigger key is pressed down (1),
@@ -160,7 +158,7 @@ void keyboard_post_init_user(void) {
 // macros
 bool process_record_user(uint16_t keycode, keyrecord_t* record) {
 
-   dprintf("KL: kc: 0x%04X, col: %2u, row: %2u, pressed: %u, time: %5u, int: %u, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
+  dprintf("KL: kc: 0x%04X, col: %2u, row: %2u, pressed: %u, time: %5u, int: %u, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
 
   switch (keycode) {
 
